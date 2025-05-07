d327a09d4830cb75530a23427d6fe104714ea9b3
Son Tung Kieu <kieusontung8@gmail.com>
2025-05-07 19:24:04 +0700

    >

--- setup.py
+++ setup.py
@@ -6,10 +6,16 @@ if __name__ == "__main__":
 # uvicorn src.search_module.app:app --port 8000
 
 # streamlit run frontend.py
-
+# https://labs.play-with-docker.com/
 # docker build -t search-api .
+# docker rm search-backend
 # docker run -d -p 8000:8000 --name search-backend search-api
 # docker ps
 # docker logs -f search-backend
+# docker save -o search-api.tar search-api:latest
+
+# https://chatgpt.com/share/681b50a2-ea38-8009-89ec-25d9672a4b07
+
+# docker load -i search-api.tar
 
 
